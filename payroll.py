1148969e108b00d2bdefc82ae6b6d7f69c87a880
Annie Kramer <akramer20@gmail.com>
2014-06-24 19:08:15 -0700

    Added employees array

--- payroll.py
+++ payroll.py
@@ -1,11 +1,16 @@
-# payroll program 
+import sys
+import os.path
+
+script, filename = sys.argv
+f = open(filename, 'r')
 
 class Person(object):
 
-    def __init__(self, name, title, wage):
+    def __init__(self, name, title, wage, hours_worked):
         self.name = name
         self.title = title
         self.wage = wage
+        self.hours_worked = hours_worked
 
     def getName(self):
         return self.name
@@ -14,14 +19,52 @@ class Person(object):
         return self.title
 
     def getWage(self):
-        return self.wage 
+        return self.wage
 
-annie = Person("annie", "programmer", "40")
-david = Person("david", "teacher", "25")
-tom = Person("tom", "programmer", "43")
-ryan = Person("ryan", "teacher", "55")
-deats = Person("deats", "programmer", "40")
+    def getHours(self):
+        return self.hours_worked
 
-print annie.getName()
-print david.getTitle()
-print ryan.getWage()    
+
+
+employees = []
+
+for line in f.readlines():
+    emp_info = line.split()
+    # print emp_info
+    # line.split() is returning arrays for each line
+    # add hours in each array for each employee
+    name = emp_info[0]
+    title = emp_info[1]
+    wage = emp_info[2]
+
+    hours_per_day = [float(h) for h in emp_info[4:10]]
+    print hours_per_day
+    hours_total = sum(hours_per_day)
+    print hours_total
+
+    new_employee = Person(name, title, wage, hours_total)
+    employees.append(new_employee)
+
+f.close()
+
+for employee in employees:
+    print employee.getName(),':'
+    print '\tTitle:', employee.getTitle()
+    print '\tWage:', employee.getWage()
+    print '\tHours Worked:', employee.getHours()
+
+
+# hw = reg_hours + ovt_hours
+
+# def classify_hours():
+#     if hw <= 40:
+#         reg_hours = 40 and ovt_hours = 0
+#     else:
+#         ovt_hours = reg_hours - 40
+
+
+
+
+# def calculate_payroll()
+# for David, 40 * 25, 10 * 35 = total hours for David
+# total expenses = all hours * rates for all employees
