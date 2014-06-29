c934de09094a937f7381d8db742e316d71e28a1e
Annie Kramer <akramer20@gmail.com>
2014-06-29 12:46:48 -0700

    Added and formatted desired output

--- payroll.py
+++ payroll.py
@@ -6,11 +6,12 @@ f = open(filename, 'r')
 
 class Person(object):
 
-    def __init__(self, name, title, wage, hours_worked):
+    def __init__(self, name, title, wage, weekday_hours, weekend_hours):
         self.name = name
         self.title = title
         self.wage = wage
-        self.hours_worked = hours_worked
+        self.weekday_hours = weekday_hours
+        self.weekend_hours = weekend_hours
 
     def getName(self):
         return self.name
@@ -21,10 +22,11 @@ class Person(object):
     def getWage(self):
         return self.wage
 
-    def getHours(self):
-        return self.hours_worked
+    def getWeekdayHours(self):
+        return self.weekday_hours
 
-
+    def getWeekendHours(self):
+        return self.weekend_hours
 
 employees = []
 
@@ -37,12 +39,14 @@ for line in f.readlines():
     title = emp_info[1]
     wage = emp_info[2]
 
-    hours_per_day = [float(h) for h in emp_info[4:10]]
-    print hours_per_day
-    hours_total = sum(hours_per_day)
+    weekday_hours = [float(h) for h in emp_info[4:9]]
+    print weekday_hours
+    weekend_hours = [float(h) for h in emp_info[9:]]
+    print weekend_hours
+    hours_total = sum(weekday_hours) + sum(weekend_hours)
     print hours_total
 
-    new_employee = Person(name, title, wage, hours_total)
+    new_employee = Person(name, title, float(wage), sum(weekday_hours), sum(weekend_hours))
     employees.append(new_employee)
 
 f.close()
@@ -51,20 +55,41 @@ for employee in employees:
     print employee.getName(),':'
     print '\tTitle:', employee.getTitle()
     print '\tWage:', employee.getWage()
-    print '\tHours Worked:', employee.getHours()
+    print '\tWeekday Hours:', employee.getWeekdayHours()
+    print '\tWeekend Hours:', employee.getWeekendHours()
 
+expenses_by_profession = {'Programmer': 0, 'Teacher': 0}
 
-# hw = reg_hours + ovt_hours
+def indiv_payroll():
+    print "===Payroll==="
+    for employee in employees:
+        weekday_reghours = employee.getWeekdayHours()
+        weekday_ovthours = 0
+        if weekday_reghours > 40:
+            weekday_ovthours = (weekday_reghours - 40)
+            weekday_reghours = 40
+        weekday_pay = weekday_reghours * employee.getWage()
+        weekend_pay = employee.getWeekendHours() * (employee.getWage() * 1.5)
+        ovt_pay = weekday_ovthours * (employee.getWage() * 1.5)
+        total_pay = weekday_pay + weekend_pay + ovt_pay
+        print employee.getName(), ": $", total_pay
 
-# def classify_hours():
-#     if hw <= 40:
-#         reg_hours = 40 and ovt_hours = 0
-#     else:
-#         ovt_hours = reg_hours - 40
+        expenses_by_profession[employee.getTitle()] += total_pay
 
+def expense_breakdown():
+    print "===Expense Breakdown==="
+    total_emp_pay = sum(expenses_by_profession.values())
+    for emp in expenses_by_profession:
+        print emp, ": $", expenses_by_profession[emp], ",", (expenses_by_profession[emp] / total_emp_pay * 100), "%"
+    print "Total: $", total_emp_pay
 
+def slackers():
+    print "==Slacker Warning!==="
+    for employee in employees:
+        total_hours = employee.getWeekdayHours() + employee.getWeekendHours()
+        if total_hours < 20:
+            print employee.getName(), "only worked for", total_hours, "hours!"
 
-
-# def calculate_payroll()
-# for David, 40 * 25, 10 * 35 = total hours for David
-# total expenses = all hours * rates for all employees
+indiv_payroll()
+expense_breakdown()
+slackers()
